7337f00901a0477e70fe7a5531a505a68095d43e
serp404 <serg.petrovich2000@gmail.com>
2021-12-05 22:52:16 +0300

    Fix installation bugs

--- hw3-TTS/init.sh
+++ hw3-TTS/init.sh
@@ -1,7 +1,7 @@
+pip install torch==1.10.0+cu111 torchaudio==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
 wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
 tar -xjf LJSpeech-1.1.tar.bz2
 mv LJSpeech-1.1/ hw_tts/data/
 mv LJSpeech-1.1.tar.bz2 hw_tts/data/
-
 git clone https://github.com/NVIDIA/waveglow.git
 pip install -r requirements.txt
