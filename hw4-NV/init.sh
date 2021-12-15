6861c106571d5477f2fba5fec41cb78908864c25
serp404 <serg.petrovich2000@gmail.com>
2021-12-16 01:35:14 +0300

    Add instalation data

---
+++ hw4-NV/init.sh
@@ -0,0 +1,7 @@
+pip install torch==1.10.0+cu111 torchaudio==0.10.0+cu111 -f https://download.pytorch.org/whl/torch_stable.html
+wget https://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
+tar -xjf LJSpeech-1.1.tar.bz2
+mkdir hw_nv/data/
+mv LJSpeech-1.1 hw_nv/data/
+mv LJSpeech-1.1.tar.bz2 hw_nv/data/
+pip install -r requirements.txt
