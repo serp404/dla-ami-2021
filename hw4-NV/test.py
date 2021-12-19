72671cf3eb4c6ce30b68cd07494c274aef00117d
serp404 <serg.petrovich2000@gmail.com>
2021-12-20 01:12:47 +0300

    Add test.py and test_data

--- hw4-NV/test.py
+++ hw4-NV/test.py
@@ -1,5 +1,4 @@
 import os
-import sys
 import argparse
 import warnings
 import random
@@ -7,14 +6,10 @@ import numpy as np
 
 import torch
 import torchaudio
-from torch.nn.utils.rnn import pad_sequence
-from torchaudio.pipelines import TACOTRON2_GRIFFINLIM_CHAR_LJSPEECH as taco
 
-from hw_tts.datasets.cleanup import basic_cleanup
-from hw_tts.melspecs import MelSpectrogramConfig
-from hw_tts.vocoders import Vocoder
-from hw_tts.models import FastSpeech
-from hw_tts.config import TaskConfig
+from hw_nv.melspecs import MelSpectrogram, MelSpectrogramConfig
+from hw_nv.models import Generator
+from hw_nv.config import TaskConfig
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
@@ -33,8 +28,8 @@ def main(args):
     outputdir_opt = pargs.outputdir
     device_opt = pargs.device
 
-    assert testpath_opt is not None, "Please, define test sentences file."
-    assert modelpath_opt is not None, "You must define pretrained model."
+    assert testpath_opt is not None, "Please, define test wavs dir."
+    assert modelpath_opt is not None, "You must define pretrained generator."
     outputdir_opt = "./" if outputdir_opt is None else outputdir_opt
 
     if device_opt is not None:
@@ -42,39 +37,21 @@ def main(args):
     else:
         DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    model = FastSpeech(**TaskConfig.model_params).to(DEVICE)
-    model = torch.load(modelpath_opt, map_location=DEVICE)
-    model.eval()
+    gen = Generator(**TaskConfig.gen_params).to(DEVICE)
+    gen.load_state_dict(torch.load(modelpath_opt, map_location=DEVICE))
+    gen.eval()
 
-    tokenizer = taco.get_text_processor()
+    test_mels = []
+    mel_featurizer = MelSpectrogram(MelSpectrogramConfig())
+    for file in sorted(os.listdir(testpath_opt)):
+        if file.endswith(".wav"):
+            wav, _ = torchaudio.load(os.path.join(testpath_opt, file))
+            test_mels.append(mel_featurizer(wav))
 
-    with open(testpath_opt, "r") as f:
-        transcripts = [s.strip() for s in f.readlines()]
-
-    tokens_list = []
-    tokens_lengths_list = []
-    for text in transcripts:
-        tokens, tokens_lengths = tokenizer(basic_cleanup(text))
-        tokens_list.append(tokens.squeeze(dim=0))
-        tokens_lengths_list.append(tokens_lengths.long())
-
-    test_batch = {
-        "tokens": pad_sequence(tokens_list, batch_first=True),
-        "tokens_lengths": torch.cat(tokens_lengths_list, dim=0),
-        "transcripts": transcripts
-    }
-
-    sys.path.append('waveglow/')
-    vocoder = Vocoder(
-        path="./hw_tts/data/waveglow_256channels_universal_v5.pt"
-    ).to(DEVICE).eval()
-
+    predicted_wavs = []
     with torch.no_grad():
-        predicted_mels, _ = model(test_batch)
-
-    predicted_wavs = vocoder.inference(
-        predicted_mels.transpose(1, 2).to(DEVICE)
-    ).cpu()
+        for mels in test_mels:
+            predicted_wavs.append(gen(mels.to(DEVICE)).cpu())
 
     for i, waveform in enumerate(predicted_wavs):
         path = os.path.join(outputdir_opt, f"test_wav_{i}.wav")
@@ -91,21 +68,21 @@ if __name__ == "__main__":
         "--modelpath",
         default=None,
         type=str,
-        help="path to tested model",
+        help="path to tested generator",
     )
     args.add_argument(
         "-t",
         "--testpath",
         default=None,
         type=str,
-        help="path to file with converted semtences",
+        help="path to file with test waveforms",
     )
     args.add_argument(
         "-o",
         "--outputdir",
         default=None,
         type=str,
-        help="dir to save output wavs",
+        help="dir to save output waveforms",
     )
     args.add_argument(
         "-d",
